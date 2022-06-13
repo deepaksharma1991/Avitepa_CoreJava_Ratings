e2c576bafd49824793d304d9756b664d3e011a34
unknown <deepakisin2020@gmail.com>
2022-06-13 13:21:51 +0530

    code commit

---
+++ README.txt
@@ -0,0 +1,3 @@
+# avitepa_corejava_ratings
+***
+This Project has been developed for computing student assignment ratings( without using any database)
---
+++ src/main/java/avitepa/corejava/ratings/exception/AssignmentCategoryException.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package avitepa.corejava.ratings.exception;
+
+/**
+ * @author Deepak S
+ *
+ */
+public class AssignmentCategoryException extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 7490883118842189156L;
+
+	public AssignmentCategoryException(String message) {
+		super(message);
+	}
+
+}
